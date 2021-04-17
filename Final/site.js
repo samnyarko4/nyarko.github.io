34d18614fe2138f87aa53b3432d4d033ea136b0a
SamNyarko <nyar0006@algonquinlive.com>
2021-04-16 23:31:13 -0400

    finished site

--- Final/site.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementsByClassName('menu-toggle')
-const navLinks = document.getElementsByClassName('navbar-links')
-
-menuToggle.addEventlistener('click' , () => { 
-    navLinks.classList.toggle('active')
-})
